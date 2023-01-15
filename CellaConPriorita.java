793faf745a276784ef6281b50aa510f627d49b97
JackSagliano <giacomosagliano@gmail.com>
2023-01-15 19:00:57 +0100

    Add files via upload

---
+++ CellaConPriorita.java
@@ -0,0 +1,11 @@
+package mnkgame;
+
+public class CellaConPriorita {
+    public MNKCell c;
+    public int priorita;
+
+    public CellaConPriorita(MNKCell c, int priorita) {
+        this.c = c;
+        this.priorita = priorita;
+    }
+}
